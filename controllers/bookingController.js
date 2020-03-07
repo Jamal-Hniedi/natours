2b31593bcedb17a2c7de62213e5ef3e13203316d
Jamal-Hniedi <jamalhniedi@gmail.com>
2020-03-08 00:14:34 +0200

    req.headers is an array not a func

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -35,20 +35,19 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
     const tour = session.client_reference_id;
-    const user = (await User.findOne({email: session.customer_email})).id;
+    const user = (await User.findOne({email: session.customer_email}))._id;
     const price = session.display_items[0].amount / 100;
     await Booking.create({tour, user, price});
 };
 
 exports.webhookCheckout = (req, res, next) => {
-    const signature = req.headers('stripe-signature');
+    const signature = req.headers['stripe-signature'];
     let event;
     try {
         event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SK);
     } catch (err) {
         return res.status().send(`Webhook error:${err.message}`)
     }
-
     if (event.type === 'checkout.session.completed')
         createBookingCheckout(event.data.object);
     res.status(200).json({received: true});
